78d8521d52eaa768bc9d00e1d4b305f57237e268
lutrapp <lutrapp@gmail.com>
2024-11-03 23:28:20 -0300

    initial commit, empty project generated by sping initializr

---
+++ solve-questions/src/main/java/com/lutrapp/solve_questions/SolveQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.lutrapp.solve_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveQuestionsApplication.class, args);
+	}
+
+}
---
+++ solve-questions/src/test/java/com/lutrapp/solve_questions/SolveQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutrapp.solve_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
